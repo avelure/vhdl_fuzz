4a48b077724b3fb65022602f3085f2b30e8ae585
Arild Velure <arild.velure@inventas.no>
2024-10-09 17:56:10 +0200

    Change fuzz command to remove warnings and stop after one error. Fuzz with dictionary. Don't remove old files when pruning. CHMOD only necessary folders. Add collection of hangs.

--- fuzz.sh
+++ fuzz.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-readonly CMD_UNDER_TEST="/usr/local/bin/ghdl -s --std=08 @@"
+readonly CMD_UNDER_TEST="/usr/local/bin/ghdl -s --std=08 -fsynopsys -frelaxed -fno-diagnostics-show-option -fno-caret-diagnostics -fpsl --mb-comments -fmax-errors=1 -Wno-library -Wno-port -Wno-pragma -Wno-specs -Wno-runtime-error -Wno-missing-wait -Wno-shared -Wno-hide -Wno-pure -Wno-analyze-assert -Wno-attribute -Wno-useless -Wno-conformance -Wno-elaboration"
+# For targets that read from a file, AFL will replace "@@" in the string with the file
+readonly CMD_UNDER_TEST_AFL="$CMD_UNDER_TEST @@"
 
 fuzz() {
+    afl-system-config
 	echo "Don't forget to update the corpus with the data from /wor/corpus_new!"
 	echo "Running fuzzer, you can start multiple containers for multithreading"
-	afl-fuzz -M ghdl -i /work/corpus -o /work/afl -- $CMD_UNDER_TEST
-	chmod -R 777 /work
+	afl-fuzz -a text -e vhd -x /vhdl_dict.txt -M ghdl -i /work/corpus -o /work/afl -- $CMD_UNDER_TEST_AFL
+	chmod -R 777 /work/afl
 }
 
 # Rename file in current directory to its hash, performs deduplication based on content
@@ -33,11 +36,12 @@ count_files () {
 
 corpus_collect () {
 	echo "Collecting corpus..."
-	rm -R /work/cur_corpus
-	mkdir /work/cur_corpus
+	mkdir -p /work/cur_corpus
 
 	# Get files from AFL queue, no name clashes since AFL has a different naming scheme
-	cp /work/afl/ghdl/queue/* /work/cur_corpus
+	cp /work/afl/ghdl/queue/id* /work/cur_corpus
+	cp /work/afl/ghdl/crashes/id* /work/cur_corpus
+	cp /work/afl/ghdl/hangs/id* /work/cur_corpus
 
 	cd /work/cur_corpus
 	parallel_rename_to_hash
@@ -49,8 +53,8 @@ corpus_collect () {
 corpus_cmin () {
 	echo "Performing corpus minimization..."
 	rm -R /work/corpus_cmin
-	mkdir /work/corpus_cmin
-	afl-cmin -i /work/cur_corpus -o /work/corpus_cmin -- $CMD_UNDER_TEST
+	mkdir -p /work/corpus_cmin
+	afl-cmin -A -T all -i /work/cur_corpus -o /work/corpus_cmin -- $CMD_UNDER_TEST_AFL
 	cd /work/corpus_cmin
 	count_files
 	cd /
@@ -62,9 +66,9 @@ corpus_tmin () {
 	mkdir -p /work/corpus_new
 	# Don't delete stuff at the tmin step since it may take ages
 
-	# find all input files, pipe to next command, {\} strips path
+	# find all input files, pipe to next command, {/} strips path
 	find /work/corpus_cmin -maxdepth 1 -type f |
-	parallel afl-tmin -i {} -o /work/corpus_new/{/} -- $CMD_UNDER_TEST
+	parallel afl-tmin -i {} -o /work/corpus_new/{/} -- $CMD_UNDER_TEST_AFL
 
 	cd /work/corpus_new
 	parallel_rename_to_hash
@@ -77,22 +81,16 @@ corpus_prune() {
 	corpus_collect
 	corpus_cmin
 	corpus_tmin
-	chmod -R 777 /work
+	chmod -R 777 /work/cur_corpus
+	chmod -R 777 /work/corpus_cmin
+	chmod -R 777 /work/corpus_new
 }
 
 crashes_collect () {
-	mkdir /work/cur_crashes
-	# Get crashes from AFL
-	cp /work/afl/ghdl/crashes/* /work/cur_crashes
-
-	cd /work/cur_crashes
-	rename_to_hash 
-	cd /
-	
 	echo "Collecting crashes"
-	mkdir /work/cur_crashes
+	mkdir -p /work/cur_crashes
 	# Get files from AFL queue, no name clashes since AFL has a different naming scheme
-	cp /work/afl/ghdl/crashes /work/cur_crashes
+	cp /work/afl/ghdl/crashes/id* /work/cur_crashes
 
 	cd /work/cur_crashes
 	parallel_rename_to_hash
@@ -103,7 +101,7 @@ crashes_collect () {
 
 crashes_cmin () {
 	echo "Deduplicating crashes..."
-	afl-cmin -C -i /work/cur_crashes -o /work/crashes_cmin -- $CMD_UNDER_TEST
+	afl-cmin -C -T all -i /work/cur_crashes -o /work/crashes_cmin -- $CMD_UNDER_TEST_AFL
 	cd /work/crashes_cmin
 	count_files
 	cd /
@@ -113,9 +111,9 @@ crashes_tmin () {
 	echo "Minimizing crashes..."
 	mkdir -p /work/crashes_new
 	
-	# find all input files, pipe to next command, {\} strips path
+	# find all input files, pipe to next command, {/} strips path
 	find /work/crashes_cmin -maxdepth 1 -type f |
-	parallel afl-tmin -i {} -o /work/crashes_new/{/} -- $CMD_UNDER_TEST
+	parallel afl-tmin -i {} -o /work/crashes_new/{/} -- $CMD_UNDER_TEST_AFL
 
 	cd /work/crashes_new
 	parallel_rename_to_hash
@@ -124,11 +122,72 @@ crashes_tmin () {
 	echo "Done"
 }
 
+crashes_report () {
+	echo "Reporting crashes..."
+	cd /work/crashes_new
+	
+	mkdir -p /work/crashes_new
+	
+	# find all input files, pipe to next command
+	find /work/crashes_new -maxdepth 1 -type f -exec $CMD_UNDER_TEST {} \; >> /work/crashes_report.txt
+
+	cd /
+	echo "Done"
+}
+
 crash_prune () {
 	crashes_collect
 	crashes_cmin
 	crashes_tmin
-	chmod -R 777 /work
+	crashes_report
+	chmod -R 777 /work/cur_crashes
+	chmod -R 777 /work/crashes_cmin
+	chmod -R 777 /work/crashes_new
+}
+
+hangs_collect () {
+	echo "Collecting hangs"
+	mkdir -p /work/cur_hangs
+	# Get files from AFL queue, no name clashes since AFL has a different naming scheme
+	cp /work/afl/ghdl/hangs/id* /work/cur_hangs
+
+	cd /work/cur_hangs
+	parallel_rename_to_hash
+	count_files
+	cd /
+	echo "Done"
+}
+
+hangs_cmin () {
+	echo "Deduplicating hangs..."
+	afl-cmin -C -T all -i /work/cur_hangs -o /work/hangs_cmin -- $CMD_UNDER_TEST_AFL
+	cd /work/hangs_cmin
+	count_files
+	cd /
+}
+
+hangs_tmin () {
+	echo "Minimizing hangs..."
+	mkdir -p /work/hangs_new
+	
+	# find all input files, pipe to next command, {\} strips path
+	find /work/hangs_cmin -maxdepth 1 -type f |
+	parallel afl-tmin -H -i {} -o /work/hangs_new/{/} -- $CMD_UNDER_TEST_AFL
+
+	cd /work/hangs_new
+	parallel_rename_to_hash
+	count_files
+	cd /
+	echo "Done"
+}
+
+hangs_prune () {
+	hangs_collect
+	hangs_cmin
+	hangs_tmin
+	chmod -R 777 /work/cur_hangs
+	chmod -R 777 /work/hangs_cmin
+	chmod -R 777 /work/hangs_new
 }
 
 helptext () {
@@ -136,6 +195,7 @@ helptext () {
 	echo "  fuzz         Run the fuzzing process"
 	echo "  corpus_prune Minimize and deduplicate the corpus for better efficiency"
 	echo "  crash_prune  Minimize and deduplicate all crashes for better debugging"
+	echo "  hangs_prune  Minimize and deduplicate all hangs for better debugging"
 }
 
 case $1 in
@@ -148,6 +208,9 @@ case $1 in
 	crash_prune)
 		crash_prune
 		;;
+	hangs_prune)
+		hangs_prune
+		;;		
 	*)
 		helptext
 		;;
